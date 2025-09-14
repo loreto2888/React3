254934cddd9f09c46f0cfd48f55956e71e90ada6
Loreto Barrientos <loreto288@gmail.com>
2025-09-14 20:11:11 -0300

    cambio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => (
         {pizzas.map(pizza => (
           <div className="col-12 col-md-6 col-lg-4" key={pizza.id}>
             <CardPizza 
+              id={pizza.id}
               name={pizza.nombre}
               price={pizza.precio}
               ingredients={pizza.ingredientes}
