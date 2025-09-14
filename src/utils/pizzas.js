1354dff6c58f85afb003bd5b4b75075728ac8cd6
Loreto Barrientos <loreto288@gmail.com>
2025-09-14 20:20:51 -0300

    imagenes

--- src/utils/pizzas.js
+++ src/utils/pizzas.js
@@ -5,42 +5,42 @@ export const pizzas = [
     id: 1,
     nombre: "Margarita",
     precio: 8000,
-    imagen: "/img/margarita.jpg",
+    imagen: "./img/margarita.jpg",
     ingredientes: ["Queso mozzarella", "Tomate", "Albahaca"]
   },
   {
     id: 2,
     nombre: "Pepperoni",
     precio: 9000,
-    imagen: "/img/pepperoni.jpg",
+    imagen: "./img/pepperoni.jpg",
     ingredientes: ["Queso mozzarella", "Pepperoni", "Tomate"]
   },
   {
     id: 3,
     nombre: "Cuatro Quesos",
     precio: 9500,
-    imagen: "/img/cuatroquesos.jpg",
+    imagen: "./img/cuatroquesos.jpg",
     ingredientes: ["Mozzarella", "Parmesano", "Gorgonzola", "Provolone"]
   },
   {
     id: 4,
     nombre: "Vegetariana",
     precio: 8500,
-    imagen: "/img/vegetariana.jpg",
+    imagen: "./img/vegetariana.jpg",
     ingredientes: ["Mozzarella", "Tomate", "Pimiento", "Aceitunas", "Cebolla"]
   },
   {
     id: 5,
     nombre: "Hawaiana",
     precio: 9000,
-    imagen: "/img/hawaiana.jpg",
+    imagen: "./img/hawaiana.jpg",
     ingredientes: ["Mozzarella", "Jamón", "Piña"]
   },
   {
     id: 6,
     nombre: "Napolitana",
     precio: 9500,
-    imagen: "/img/napolitana.jpg",
+    imagen: "./img/napolitana.jpg",
     ingredientes: ["Mozzarella", "Tomate", "Anchoas", "Orégano"]
   }
 ];
