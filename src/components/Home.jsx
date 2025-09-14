1b728f54b019fbf735f520a1487a3e5262e4df9b
Loreto Barrientos <loreto288@gmail.com>
2025-09-14 20:44:34 -0300

    cambios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,26 +2,31 @@
 import Header from './Header';
 import CardPizza from './CardPizza';
 import { pizzas } from '../utils/pizzas';
+import { useCart } from '../context/CartContext';
 
-const Home = () => (
-  <main>
-    <Header />
-    <section className="container py-4">
-      <div className="row g-4">
-        {pizzas.map(pizza => (
-          <div className="col-12 col-md-6 col-lg-4" key={pizza.id}>
-            <CardPizza 
-              id={pizza.id}
-              nombre={pizza.nombre}
-              precio={pizza.precio}
-              ingredientes={pizza.ingredientes}
-              imagen={pizza.imagen}
-            />
-          </div>
-        ))}
-      </div>
-    </section>
-  </main>
-)
+const Home = () => {
+  const { addToCart } = useCart();
+  return (
+    <main>
+      <Header />
+      <section className="container py-4">
+        <div className="row g-4">
+          {pizzas.map(pizza => (
+            <div className="col-12 col-md-6 col-lg-4" key={pizza.id}>
+              <CardPizza 
+                id={pizza.id}
+                nombre={pizza.nombre}
+                precio={pizza.precio}
+                ingredientes={pizza.ingredientes}
+                imagen={pizza.imagen}
+                onAddToCart={() => addToCart(pizza)}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
 
 export default Home
