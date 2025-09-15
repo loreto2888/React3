e598be473c036abf1c1b421c35eabfda6097924b
Loreto Barrientos <loreto288@gmail.com>
2025-09-14 21:41:43 -0300

    cambios

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React2/',
+  base: '/React3/',
   plugins: [react()],
   build: {
     outDir: 'docs',
